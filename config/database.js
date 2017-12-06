e4e39ec79bd9f768cdd881228f09faa0393aa112
root <root@reveries-project.fr>
2017-12-06 15:42:27 +0000

    tuto

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 	// config/database.js
 module.exports = [{
 
-    'url': 'mongodb://conception:reveriesconception@localhost/test?authSource=admin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://conception:reveriesConception@localhost/test?authSource=test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 }, { 'url': 'mongodb://localhost/game' }];
 	 
